be629a70d577288904ebfea729b4f62a51c391b8
Zfanz12 <zulfanfalah@gmail.com>
2026-04-06 04:47:34 +0700

    commit message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_MODUL6_103022400076
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SayaPlayTrack track = new SayaPlayTrack("Bohemian Rhapsody", 500);
+
+            Console.WriteLine("Sebelum ditambah:");
+            track.PrintTrackDetails();
+
+            track.increasePlayCount(200);
+
+            Console.WriteLine("Sesudah ditambah 200:");
+            track.PrintTrackDetails();
+
+
+            try
+            {
+                SayaPlayTrack trackBesar = new SayaPlayTrack("Lagu Viral", 99_000_000);
+                trackBesar.increasePlayCount(5_000_000); 
+            }
+            catch (OverflowException e)
+            {
+
+                Console.WriteLine("\n[EXCEPTION TERTANGKAP]: " + e.Message);
+            }
+
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ SayaPlayTrack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL6_103022400076 
+{
+    class SayaPlayTrack
+    {
+        private string title;       
+        private int playCount;     
+
+        public SayaPlayTrack(string title, int playCount)
+        {
+            if (title == null)
+                throw new ArgumentException("Title tidak boleh null!");
+
+            if (playCount > 100_000_000)
+                throw new ArgumentException("Play count melebihi batas maksimal (100 juta)!");
+
+            this.title = title;
+            this.playCount = playCount;
+        }
+        public void increasePlayCount(int count)
+        {
+
+            if (this.playCount + count > 100_000_000)
+                throw new OverflowException("Total play count tidak boleh melebihi 100 juta!");
+
+            this.playCount += count; 
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("=== TRACK DETAILS ===");
+            Console.WriteLine("Title     : " + this.title);
+            Console.WriteLine("Play Count: " + this.playCount);
+            Console.WriteLine("=====================");
+        }
+    }
+}
